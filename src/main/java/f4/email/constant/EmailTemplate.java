3601cc16f022793289b0f098975c04215ebe63c4
Jiwoon <kfromh0136@Naver.com>
2023-08-23 16:24:30 +0900

    [#3] [feat] email Template html file
    이메일 알람을 받아야 하는 두 가지 경우의 html 파일을 작성하였음.
    이메일 인증코드 -> AuthenticationCode.html
    낙찰 사실 알림 -> SuccessfulBid.html

---
+++ src/main/java/f4/email/constant/EmailTemplate.java
@@ -0,0 +1,16 @@
+package f4.email.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EmailTemplate {
+    SOURCE_EMAIL("kfromh0136@gmail.com"),
+    TEMPLATE_PATH("classpath:templates/"),
+    AUTHENTICATION_CODE_EMAIL_SUBJECT("[ArteModerni] 이메일 인증 코드입니다."),
+    AUTHENTICATION_CODE_EMAIL_TEMPLATE("AuthenticationCode.html"),
+    SUCCESSFUL_BID_EMAIL_SUBJECT("[ArteModerni] 축하합니다. 입찰하신 상품이 낙찰 되었음을 알려드립니다."),
+    SUCCESSFUL_BID_EMAIL_TEMPLATE("SuccessfulBid.html");
+    private final String value;
+}
